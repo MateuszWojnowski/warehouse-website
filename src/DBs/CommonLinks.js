413acbae42636b8cc25a43d4f2b326509a441321
Mateusz Wojnowski <jimo500@wp.pl>
2021-09-09 21:14:33 +0200

    aktualizacja linkow wrzesien

--- src/DBs/CommonLinks.js
+++ src/DBs/CommonLinks.js
@@ -6,8 +6,8 @@ let eDec = "https://decathlon.hcmdeck.com/protected/home"
 let logbi = "https://live.decathlon.net/dashboard/monitor5/w058"
 let planTransportowy = "https://docs.google.com/spreadsheets/d/18JHoKptThjwl8pA68zbJjvUjcbXE9w6aUvcZimEvCXM/edit?usp=drive_web&ouid=113865301763495915815&urp=gmail_link&gxids=7628" /*2021*/
 let kartaSynchronizacji = "https://docs.google.com/spreadsheets/d/1GjLj1yH_-oIRCXwkFWQfty4GN-q9VOch_kVc6111Puo/edit?ts=5fecdd9b#gid=1372544627" /*2021*/
-let OBS = "https://docs.google.com/spreadsheets/d/1k-nVyCCj68lFmKVs_kSvlb0t-gOqmTCkbNuKd9RExrE/edit" /*sierpien*/
-let OBSTygodniowy = "https://docs.google.com/spreadsheets/d/1CXFQw_oW-7tgOR3wPwndyK58XCCR2p7GRY4G1nHozKQ/edit#gid=0" /*lipiec i sierpien*/
+let OBS = "https://docs.google.com/spreadsheets/d/1NXIal5WUrXX342gfP2gD4BQRD7YQNMdWeow2qyPfUmM/edit#gid=365144502" /*sierpien*/
+let OBSTygodniowy = "https://docs.google.com/spreadsheets/d/1UJEpBAJ628t5GMs9Tvcb8VS6dATi9hjsZZF9EFYrUVU/edit" /*lipiec i sierpien*/
 let kalkulatorLidera = "http://gliwn090/kalkulator-lidera/"
 let zglosUsterke = "https://docs.google.com/forms/d/e/1FAIpQLSeGGU5ap9CimgFM0RIuKeB_EhOozMDuEIypnfYPJhUques5eQ/viewform?gxids=7628"
 let teta = "https://hrm3.hrk.pl/decathlon/login.html"
